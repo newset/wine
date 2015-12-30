dc91d2263517796c0a75ebcff8d13f34be029fbb
Insane <910599438@qq.com>
2015-12-31 00:32:28 +0800

    调试

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,22 @@
         <link rel="stylesheet" href="lib/ng-dialog/css/ngDialog-theme-default.min.css">
         <link rel="stylesheet" href="css/ngDialog-theme-flat.css">
         <link rel="stylesheet" href="css/style.css">
+        <script>
+            var phoneWidth = parseInt(window.screen.width);
+            var phoneScale = phoneWidth/640;
+            var ua = navigator.userAgent;
+            if (/Android (\d+\.\d+)/.test(ua)){
+                var version = parseFloat(RegExp.$1);
+                if(version>2.3){
+                    document.write('<meta name="viewport" content="width=640, minimum-scale = '+phoneScale+', maximum-scale = '+phoneScale+', target-densitydpi=device-dpi">');
+                }else{
+                    document.write('<meta name="viewport" content="width=640, target-densitydpi=device-dpi">');
+                }
+            } else {
+                document.write('<meta name="viewport" content="width=640, user-scalable=no, target-densitydpi=device-dpi">');
+            }
+
+        </script>
         <script type="text/javascript">
             window.userinfo = '{{ $user ? 'ok' : ''}}';
             window.baseUrl = '{{url('/')}}'
